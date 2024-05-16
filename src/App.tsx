1820115084164d3b8bc12a77ca4935347ad16d77
sudnyesh136 <158823406+sudnyesh136@users.noreply.github.com>
2024-05-17 00:56:31 +0530

    asdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { TonConnectButton } from "@tonconnect/ui-react";
+
 import { Counter } from "./components/Counter";
 import { Jetton } from "./components/Jetton";
 import { TransferTon } from "./components/TransferTon";
@@ -8,7 +9,7 @@ import { Button, FlexBoxCol, FlexBoxRow } from "./components/styled/styled";
 import { useTonConnect } from "./hooks/useTonConnect";
 import { CHAIN } from "@tonconnect/protocol";
 import "@twa-dev/sdk";
-
+import Game from "./game/home";
 const StyledApp = styled.div`
   background-color: #e8e8e8;
   color: black;
@@ -28,26 +29,17 @@ const AppContainer = styled.div`
 
 function App() {
   const { network } = useTonConnect();
+  
 
   return (
+
     <StyledApp>
       <AppContainer>
         <FlexBoxCol>
-          <FlexBoxRow>
             <TonConnectButton />
-            <Button>
-              {network
-                ? network === CHAIN.MAINNET
-                  ? "mainnet"
-                  : "testnet"
-                : "N/A"}
-            </Button>
-          </FlexBoxRow>
-          <Counter />
-          <TransferTon />
-          <Jetton />
         </FlexBoxCol>
       </AppContainer>
+      <Game />
     </StyledApp>
   );
 }
---
+++ src/game/home.tsx
@@ -0,0 +1,15 @@
+// App.js
+
+import React from 'react';
+
+function Game() {
+  return (
+    <div className="container">
+      <h1>Hello, React!</h1>
+      <p>This is a basic React template with some design.</p>
+      <button className="btn">Click Me</button>
+    </div>
+  );
+}
+
+export default Game;
